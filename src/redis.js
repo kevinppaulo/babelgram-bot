f70087234a4f71152393d28080a37f7ed428a647
Kevin Paulo <kevinppaulo@gmail.com>
2021-04-23 14:20:19 -0300

    use redis togo url

--- src/redis.js
+++ src/redis.js
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 import JSONCache from "redis-json";
 
-const redis = new Redis();
+const redis = new Redis(process.env.REDISTOGO_URL);
 const jsonCache = new JSONCache(redis, { prefix: "cache" });
 
 export default jsonCache;
