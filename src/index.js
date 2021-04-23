93840680d7973bf9962faed15afc0c89e6e4c4bc
Kevin Paulo <kevinppaulo@gmail.com>
2021-04-23 13:32:04 -0300

    returning index.html

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import onMessageReceived from './webhook.js';
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import onMessageReceived from "./webhook.js";
+import path from "path";
 
 // config
 dotenv.config();
 const app = express();
 const appSecretToken = process.env.WEBHOOK_SECRET;
+const rootPath = path.resolve(process.cwd());
+
 app.use(bodyParser.json());
 
 // start listenning
 app.listen(process.env.APP_PORT, () => {
-	console.log(`Example app listening at http://localhost:${process.env.APP_PORT}`);
+    console.log(`Example app listening at http://localhost:${process.env.APP_PORT}`);
 });
 
 // endpoints
-app.post('/' + appSecretToken + '/update', onMessageReceived);
+app.post("/" + appSecretToken + "/update", onMessageReceived);
 
-app.use(express.static('./media/synthesized/'));
+app.use(express.static("./media/synthesized/"));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+app.get("/", function (req, res) {
+    const path = `${rootPath}/src/index.html`;
+    res.sendFile(path);
 });
